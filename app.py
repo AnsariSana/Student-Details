fafe84c20d030d142b0336b7c7ad906c30805f10
AS-Coder26 <ascoder26@gmail.com>
2019-09-24 14:56:58 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template,flash, redirect,url_for,session,logging,request
+from pymongo import MongoClient 
+
+
+app = Flask(__name__)
+app.secret_key = "abc"
+try: 
+    conn = MongoClient() 
+    print("Connected successfully!!!") 
+except:   
+    print("Could not connect to MongoDB") 
+
+db = conn.database
+
+collection = db.student
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/login",methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        uname = request.form["uname"]
+        passw = request.form["passw"]
+        #print(uname)
+        ename = collection.find_one({'email' : uname,'password':passw})
+        #print(ename)
+        if ename:
+        	session['email'] = uname
+        	return redirect(url_for("show", email = session['email']))
+        else:
+        	#flash("enter correct credentials")
+        	return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form['name']
+        rollno = request.form['rollno']
+        mail = request.form['mail']
+        passw = request.form['passw']
+        std = request.form['std']
+
+
+        rec_id1 = collection.insert({'name' : name,
+        	'rollno' : rollno,
+        	'email' : mail,
+        	'password' : passw,
+        	'std' : std,
+        	'grades':[]}) 
+        print("Data inserted with record ids",rec_id1) 
+        return redirect(url_for("getgrades",std = std,mail = mail))
+    return render_template("register.html")
+
+@app.route("/show/<email>")
+def show(email,methods = ["GET", "POST"]):
+ 	print(email)
+ 	data = collection.find_one({"email" : email})
+
+ 	return render_template("show.html", name = data['name'],rollno = data['rollno'], email = data['email'], grades = data['grades'], length = len(data['grades']))
+
+
+@app.route("/getgrades/<int:std>/<mail>",methods=["GET", "POST"])
+def getgrades(std,mail):
+	print(std)
+	if request.method == "POST":
+		for i in range(1,std):
+			stnd = request.form['std'+str(i)]
+			
+			grade = request.form['grade'+str(i)]
+			remark = request.form['remark'+str(i)]
+			percentage = request.form['percentage'+str(i)]
+			print(stnd,grade,remark,percentage)
+			response = collection.update({"email" : mail}, {"$push" : {"grades" : {"stnd" :stnd,"grade":grade,"remark":remark,"percentage":percentage}}})
+			#response = rec.grades = {"stnd" :stnd,"grade":grade,"remark":remark,"percentage":percentage}
+			print(response)
+		return redirect(url_for("login"))
+ 
+
+	return render_template("grades.html",std = std)
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
+#app.run(port=5000)
